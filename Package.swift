e7704973488f0b6b29ba5cfa38faf0ccc0ae8fad
William Alexander <wwalexander@users.noreply.github.com>
2024-11-04 17:20:16 -0500

    Rewrite

--- Package.swift
+++ Package.swift
@@ -3,15 +3,34 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-semantic-versioning",
+    name: "SemanticVersioning",
+    platforms: [
+        .macOS(.v10_15),
+        .macCatalyst(.v13),
+        .iOS(.v13),
+    ],
     products: [
         .library(
             name: "SemanticVersioning",
-            targets: ["SemanticVersioning"]),
+            targets: ["SemanticVersioning"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/pointfreeco/swift-parsing",
+            .upToNextMajor(from: "0.13.0")
+        ),
     ],
     targets: [
         .target(
-            name: "SemanticVersioning"),
+            name: "SemanticVersioning",
+            dependencies: [
+                .product(
+                    name: "Parsing",
+                    package: "swift-parsing"
+                ),
+            ]
+        ),
         .testTarget(
             name: "SemanticVersioningTests",
             dependencies: ["SemanticVersioning"]
